8003c1c0cb157901be1ca09fde10b17ce07e3bce
Jani Eväkallio <jani.evakallio@gmail.com>
2023-07-27 23:47:10 +0100

    Remove legacy chat implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { App } from "@/components/App";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <App />
+    <main className="p-4">
+      <Link href="/chat" className="underline">
+        Chat
+      </Link>
     </main>
   );
 }
--- party/main.ts
+++ party/main.ts
@@ -1,63 +1,7 @@
-import { PartyKitConnection, PartyKitServer } from "partykit/server";
-import { User, authenticateUserSession, getUserSession } from "./utils/auth";
-
-type MessageEvent =
-  | {
-      type: "identify";
-      sessionToken: string;
-      csrfToken: string;
-      username: string;
-    }
-  | { type: "message"; text: string; sender: User }
-  | { type: "error"; text: string };
-
-type Connection = PartyKitConnection & {
-  username?: string;
-};
+import { PartyKitServer } from "partykit/server";
 
 export default {
-  onConnect(connection: Connection) {
-    connection.username = (connection.deserializeAttachment() ?? {}).username;
-  },
-
-  async onMessage(message, connection: Connection, room) {
-    const reply = (response: MessageEvent) =>
-      connection.send(JSON.stringify(response));
-
-    const event = JSON.parse(message as string) as MessageEvent;
-
-    if (event.type === "identify") {
-      const user = await authenticateUserSession(room, event);
-      if (user) {
-        connection.username = user.username;
-        connection.serializeAttachment({
-          ...(connection.deserializeAttachment() ?? {}),
-          username: user.username,
-        });
-
-        reply({
-          type: "message",
-          text: `Welcome ${user.username}`,
-          sender: {
-            username: "system",
-          },
-        });
-      } else {
-        reply({ type: "error", text: "No user found" });
-      }
-    }
-
-    if (event.type === "message") {
-      const user = connection.username
-        ? await getUserSession(room, connection.username)
-        : null;
-
-      if (user) {
-        const message = { ...event, sender: user };
-        room.broadcast(JSON.stringify(message));
-      } else {
-        reply({ type: "error", text: "Unauthenticated" });
-      }
-    }
+  onConnect() {
+    // TODO: Implement
   },
 } satisfies PartyKitServer;
