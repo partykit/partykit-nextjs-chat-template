80163775026a326a113a59c1bb0df5f6bebffebc
Matt Webb <matt@interconnected.org>
2023-08-02 14:02:59 +0100

    HTML fix

--- app/chat/NewRoom.tsx
+++ app/chat/NewRoom.tsx
@@ -11,11 +11,11 @@ export default function NewRoom(props: { slug: string}) {
     }
 
     return (
-        <p className="mt-6 flex flex-row justify-start items-center gap-2">
-            <div>Create a new room: <span className="font-medium bg-yellow-50">{slug}</span>.</div>
+        <div className="mt-6 flex flex-row justify-start items-center gap-2">
+            <p>Create a new room: <span className="font-medium bg-yellow-50">{slug}</span>.</p>
             <form onSubmit={handleClick}>
                 <button type="submit" className="bg-stone-200 hover:bg-stone-300 px-2 py-1 rounded">Enter -&gt;</button>
             </form>
-        </p>
+        </div>
     )
 }
