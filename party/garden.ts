16ccf5bf02785c8f36247cca9ef0eaa5a3efc409
Matt Webb <matt@interconnected.org>
2023-08-03 09:57:18 +0100

    Iterate the garden

--- party/garden.ts
+++ party/garden.ts
@@ -1,6 +1,10 @@
-import { PartyKitServer } from "partykit/server";
+import { PartyKitServer, PartyKitRoom } from "partykit/server";
 import { onConnect } from "y-partykit";
 import { syncedStore, getYjsDoc } from "@syncedstore/core";
+
+type YJsRoom = PartyKitRoom & {
+    store?: any;
+}
 
 type Lineage = string[];
 
@@ -33,7 +37,7 @@ const lineages = {
     squirrel: ["🌰", "🌰", "🌰", "🐿️", "🐿️"],
     sprout: ["🌱", "🌱", "🌱", "🌱", "🍄", "🍄", "🍄", "🦌", "🦌"],
     flower: ["🌱", "🌱", "🌸", "🌸", "🌸", "🌸", "🌸", "🌸", "🐝", "🐝"],
-    fern: ["🌿", "🌿", "🌿", "🌿", "🌿", "🌿", "🌿", "🌿", "🐻", "👣", "👣", "👣"]
+    fern: ["🌿", "🌿", "🌿", "🌿", "🌿", "🌿", "🌿", "🌿", "🐻", "🐻", "👣", "👣"]
 } as Lineages;
 
 export const yDocShape = { garden: {} as Garden };
@@ -52,13 +56,29 @@ export const getStarterEmojis = () => {
     }) as Cell[];
 }
 
+const GARDEN_TICK = 1500; // milliseconds
+
 export default {
     onConnect(ws, room) {
         return onConnect(ws, room, {
             callback: {
                 async handler(ydoc) {
                     try {
-                        // @TODO Stash the ydoc and set an alarm if it isn't empty
+                        // Stash the ydoc and set an alarm if it isn't empty
+                        if (!(room as YJsRoom).store) {
+                            const store = syncedStore(yDocShape, ydoc);
+                            //console.log("Stashing store", store);
+                            (room as YJsRoom).store = store;
+                        }
+                        // If there's no alarm set, set one for the next tick
+                        const alarm = await room.storage.getAlarm();
+                        //console.log("alarm", alarm);
+                        if (alarm === null) {
+                            //console.log("Setting alarm");
+                            await room.storage.setAlarm(
+                                new Date().getTime() + GARDEN_TICK
+                            );
+                        }
                     } catch (e) {
                         console.error("Callback error", e);
                     }
@@ -67,6 +87,33 @@ export default {
         });
     },
     async onAlarm(room) {
-        // @TODO iterate the garden and set a new alarm if the garden isn't empty
+        //console.log("onAlarm: iterating garden");
+        // iterate the garden and set a new alarm if the garden isn't empty
+        const store = (room as YJsRoom).store;
+        Object.entries(store.garden as Garden).forEach(([index, cell]) => {
+            if (cell) {
+                // Check the lineage and index against the lineages map
+                // If the index can be incremented, increment it and update the emoji.
+                // If it can't be incremented, remove the cell from the garden.
+                const lineage = lineages[cell.lineage];
+                if (lineage) {
+                    if (cell.index < lineage.length - 1) {
+                        cell.index++;
+                        cell.emoji = lineage[cell.index];
+                    } else {
+                        delete store.garden[index];
+                    }
+                } else {
+                    delete store.garden[index];
+                }
+            }
+        });
+
+        // Set the alarm if the garden isn't empty
+        if (store.garden.size > 0) {
+            await room.storage.setAlarm(
+                new Date().getTime() + GARDEN_TICK
+            );
+        }
     }
 } satisfies PartyKitServer;
