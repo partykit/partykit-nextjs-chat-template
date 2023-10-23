da208c2cbd0a65cbcf14c77574e5c2c96fc2c73c
Jani Eväkallio <jani.evakallio@gmail.com>
2023-10-23 22:55:12 +0100

    Remove unnecessary code

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,25 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { NextRequest } from "next/server";
 import { PARTYKIT_URL } from "@/app/env";
-
-const secret = process.env.NEXTAUTH_SECRET;
-
-export async function GET(req: NextRequest) {
-  const sessionToken = await getToken({ req, secret, raw: true });
-  if (!sessionToken) {
-    return NextResponse.json({ error: "No token" }, { status: 401 });
-  }
-
-  const session = await getToken({ req, secret });
-  if (!session) {
-    return NextResponse.json({ error: "No session" }, { status: 401 });
-  }
-
-  return NextResponse.json({
-    sessionToken,
-    session,
-  });
-}
 
 export async function POST(req: NextRequest) {
   const roomId = req.nextUrl.searchParams.get("room");
